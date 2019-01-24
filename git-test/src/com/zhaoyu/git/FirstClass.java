4657681e35efb96523463c927ab74051ed8a6b5c
zhaoyu <94742242@qq.com>
2019-01-24 23:56:50 +0800

    修改

--- git-test/src/com/zhaoyu/git/FirstClass.java
+++ git-test/src/com/zhaoyu/git/FirstClass.java
@@ -3,6 +3,7 @@ package com.zhaoyu.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println();
+        //hello test
     }
 
     public void test() {
