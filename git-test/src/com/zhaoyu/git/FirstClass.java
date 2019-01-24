e89859ca574a5f0d3cba011d1705a8bb63987470
zhaoyu <94742242@qq.com>
2019-01-24 23:49:19 +0800

    把本地工程添加到git

---
+++ git-test/src/com/zhaoyu/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.zhaoyu.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public void test() {
+        int i = 0;
+        i += 10;
+    }
+}
